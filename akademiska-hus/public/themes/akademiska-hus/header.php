c373a3bfbf8c0aa1a2f28a415f55734708014c51
andrerobsahm <andrerobsahm@users.noreply.github.com>
2018-05-07 15:27:24 +0200

    added react | added manifest.json

--- akademiska-hus/public/themes/akademiska-hus/header.php
+++ akademiska-hus/public/themes/akademiska-hus/header.php
@@ -4,6 +4,7 @@
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="theme-color" content="#6d9aea">
+  <link rel=”manifest” href=”/manifest.json”>
 
   <?php wp_head(); ?>
 </head>
