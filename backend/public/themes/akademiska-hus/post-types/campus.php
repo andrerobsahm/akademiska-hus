e3f2cbaabe44f38ecdb43d7359d9e4ad66dcee68
besv16 <beasve0506@skola.goteborg.se>
2018-05-21 17:52:12 +0200

    Starting to change loading bar behavior etc

---
+++ backend/public/themes/akademiska-hus/post-types/campus.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+add_action('init', function () {
+    register_post_type('campus', [
+        'has_archive' => true,
+        'labels' => [
+            'add_new_item' => __('Lägg till campus'),
+            'edit_item' => __('Ändra ett campus'),
+            'name' => __('Campus'),
+            'search_items' => __('Sök campus'),
+            'singular_name' => __('campus'),
+        ],
+        'menu_icon' => 'dashicons-admin-multisite',
+        'menu_position' => 20,
+        'public' => true,
+    ]);
+});
