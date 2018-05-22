b39eb990eeca3a63ac5d23756bf4fb94cc620c1c
andrerobsahm <andrerobsahm@users.noreply.github.com>
2018-05-22 10:36:20 +0200

    changes

--- frontend/src/Components/Header/index.js
+++ frontend/src/Components/Header/index.js
@@ -5,7 +5,7 @@ import logo from "../../logo.svg";
 const Header = () => (
     <div className="header">
         <img src={logo} alt="A-hus logotype" className="app-logo" />
-        <p className="header-title">Akademiska hus</p>
+        <h1 className="header-title">Akademiska hus</h1>
     </div>
 );
 
