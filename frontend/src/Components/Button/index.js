abcde08fbe0e1ef48b823a106b80f07b5354b3e1
besv16 <beasve0506@skola.goteborg.se>
2018-05-27 21:52:51 +0200

    Chosen campus buttons added, created Chosen campus folder with its own css, etc.

--- frontend/src/Components/Button/index.js
+++ frontend/src/Components/Button/index.js
@@ -6,7 +6,7 @@ const Button = props => (
     <a href="{props.link}">
         <button className="button">
             <span>
-                <img src="props.icon" alt=""/>
+                <img src="{props.icon}" alt=""/>
             </span>
             {props.title}
         </button>
